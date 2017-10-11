227f0839230c69a7c711a850a4226c87f3fa2080
oliverlai55 <oliverlai55@gmail.com>
2017-10-11 22:12:54 +0000

    Add new tickets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "projects#index"
   
-  resources :projects
+  resources :projects do
+    resources :tickets
+  end
   # this allows routes to be configed by Rails
   
   
