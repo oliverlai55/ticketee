1cf71eabc53a6e10917d2ca823797104fa816fbe
oliverlai55 <oliverlai55@gmail.com>
2017-10-12 21:58:03 +0000

    Passed test

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature "Users can create new ticket" do
   scenario "when providing invalid attributes" do
     click_button "Create Ticket"
     
-    expect(page).to have_content "Ticket has not created"
+    expect(page).to have_content "Ticket has not been created."
     expect(page).to have_content "Name can't be blank"
     expect(page).to have_content "Description can't be blank"
   end
