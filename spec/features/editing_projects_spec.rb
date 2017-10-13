44208ed72ba7c67b414204b015579cd81a1538de
oliverlai55 <oliverlai55@gmail.com>
2017-10-13 21:11:43 +0000

    Write new test for creating project

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -9,11 +9,19 @@ RSpec.feature "Users can edit existing projects" do
     click_link "Edit Project"
   end
   
-  scenario "with valid attriutes" do
+  scenario "with valid attributes" do
     fill_in "Name", with: "Sublime Text 4 beta"
     click_button "Update Project"
     
     expect(page).to have_content "Project has been updated."
     expect(page).to have_content "Sublime Text 4 beta"
   end
+  
+  scenario "when providing invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+    
+    expect(page).to have_content "Project has not been updated."
+  end
+  
 end
