51896b066ddb2bf4a495df05517342698aefc48a
oliverlai55 <oliverlai55@gmail.com>
2017-10-27 18:33:46 +0000

    Add admin namespace with application controller

--- spec/controllers/admin/application_controller_spec.rb
+++ spec/controllers/admin/application_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Admin::ApplicationController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
   
   before do
+    allow(controller).to receive(:authenticate_user!)
     allow(controller).to receive(:current_user).and_return(user)
   end
   
