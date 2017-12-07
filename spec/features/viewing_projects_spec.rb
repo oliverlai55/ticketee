d31008eff50bb5f17c1f421264b5ab2d54f10e55
oliverlai55 <oliverlai55@gmail.com>
2017-12-07 23:18:10 +0000

    Add new tests

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Users can view projects" do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project, name: "Sublime Text 3") }
+  
+  before do
+    login_as(user)
+    assign_role!(user, :viewer, project)
+  end
   
   scenario "with the project details" do
     project = FactoryGirl.create(:project, name: "Sublime Text 3")
